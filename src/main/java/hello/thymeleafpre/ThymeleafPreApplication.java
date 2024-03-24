7cb8bc2b192f8eb42fa990f6d8b78fdb10d3e9e9
lby132 <quddbscjswow@naver.com>
2024-03-24 23:11:19 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleafpre/ThymeleafPreApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleafpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymeleafpre/ThymeleafPreApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleafpre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
